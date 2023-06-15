5d6500d4b54c1127580cee9f3cf59a75f1792638
tonpm14 <uo266575@uniovi.es>
2023-06-15 13:40:57 +0200

    Initial commit

---
+++ src/main/java/com/carfinder/carfinder/CarFinderApplication.java
@@ -0,0 +1,13 @@
+package com.carfinder.carfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarFinderApplication.class, args);
+    }
+
+}
